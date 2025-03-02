cf809098d1e121a6dc4ffe9723127c11f17dded3
chairil0303 <chairilsyahrain24@gmail.com>
2025-03-02 22:27:13 +0700

    membuat controller api

--- database/migrations/2025_03_02_151341_create_employees_table.php
+++ database/migrations/2025_03_02_151341_create_employees_table.php
@@ -3,19 +3,22 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use  Illuminate\Database\QueryException ;
 
 return new class extends Migration
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('employees', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    Schema::create('employees', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('position');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
